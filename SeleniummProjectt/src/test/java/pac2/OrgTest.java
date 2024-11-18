3f63f27159737063e10c20c3901dda6d508ec1f2
DELL <DELL@DESKTOP-7H03NUQ>
2024-11-18 15:56:27 +0530

    first push

---
+++ SeleniummProjectt/src/test/java/pac2/OrgTest.java
@@ -0,0 +1,26 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	@Test
+	public void createOrg() {
+		String URL= System.getProperty("url");
+		String BROWSER=System.getProperty("browser","chrome");
+		String USERNAME=System.getProperty("username");
+		String PASSWORD=System.getProperty("password");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+		System.out.println("execute createOrgTest");
+	}
+	@Test
+	public void modifyOrg()
+	{
+		System.out.println("execute modifyOrgTest");
+	}
+
+}
+
